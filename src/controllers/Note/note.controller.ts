7a41de71946795f9a2019b6bcbf61b767b382c1e
umpierres <alexandraumpierres@outlook.com>
2023-08-01 16:18:28 -0300

    criando delete e update

--- src/controllers/Note/note.controller.ts
+++ src/controllers/Note/note.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express"
-import { CreateNote } from "../../usecases";
+import { CreateNote, UpdateNote } from "../../usecases";
+import { ListNotes } from "../../usecases/Notes/listNote.usecase";
 
 export class NoteController {
 
@@ -8,7 +9,20 @@ export class NoteController {
 
         const usecase = new CreateNote();
 
-        const response = usecase.execute({ title, description, owner,archived , favorite})
+        const response = usecase.execute({ title, description, owner, archived , favorite})
+
+        if(!response.success){
+            return res.status(400).json(response);
+        }
+
+        return res.status(201).json(response);
+    }
+    list(req: Request, res:Response) {
+        const {owner} = req.body
+
+        const usecase = new ListNotes();
+
+        const response = usecase.execute(owner)
 
         if(!response.success){
             return res.status(400).json(response);
@@ -17,9 +31,31 @@ export class NoteController {
         return res.status(201).json(response);
     }
 
-    delete(){}
+    update(req: Request, res:Response){
+        const {id, data} = req.body
 
-    update(){}
+        const usecase = new UpdateNote();
 
-    list() {}
+        const response = usecase.execute(id, data)
+
+        if(!response.success){
+            return res.status(400).json(response);
+        }
+
+        return res.status(201).json(response);
+    }
+
+    delete(req: Request, res:Response){
+        const {id} = req.body
+
+        const usecase = new ListNotes();
+
+        const response = usecase.execute(id)
+
+        if(!response.success){
+            return res.status(400).json(response);
+        }
+
+        return res.status(201).json(response);
+    }
 }
