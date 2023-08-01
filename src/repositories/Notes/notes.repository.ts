3a3d009078ac60ba36ec17513f1ed93986b1ebab
umpierres <alexandraumpierres@outlook.com>
2023-08-01 16:18:41 -0300

    add delete update

--- src/repositories/Notes/notes.repository.ts
+++ src/repositories/Notes/notes.repository.ts
@@ -1,30 +1,40 @@
 import { notes } from '../../database';
 import { Note, User } from '../../classes';
-import { NoteDTO } from '../../usecases';
+import { CreateNoteDTO, UpdateNoteDTO } from '../../usecases';
 
 export class NoteRepository {
+    //ok
+    createNote(dados: CreateNoteDTO) {
+            const note = new Note(dados.title, dados.description, dados.owner, dados.archived, dados.favorite);
+    
+            notes.push(note);
+            return note;
+    }
+    //ok
     listNotes(owner: User) {
         const currentUserNotes = notes.filter((note) => note.toJson().owner === owner);
 
         return currentUserNotes;
     }
 
-    createNote(dados: NoteDTO) {
-        const note = new Note(dados.title, dados.description, dados.owner, dados.archived, dados.favorite);
-
-        notes.push(note);
-        return note;
-    }
-
-    updateNote(id: string, dados: NoteDTO) {
+    updateNote(id: string, dados: UpdateNoteDTO) {
         const noteIndex = notes.findIndex((note) => note.getId() === id);
     
         if (noteIndex === -1) {
             throw new Error("Nota não encontrada.");
         }
-    
+
+        if(!dados.description || !dados.title){
+            throw new Error("Campo obrigatorio.");
+        }
+
+        notes[noteIndex].setTitle(dados.title);
+        notes[noteIndex].setDescription(dados.description);
+
+        return notes[noteIndex]
     }
 
+    //ok
     deleteNote(id: string) {
         const noteIndex = notes.findIndex((note) => note.getId() === id);
 
