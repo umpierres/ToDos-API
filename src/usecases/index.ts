d1431f8303cab1a1866083a0e70ab45ad91a6880
umpierres <alexandraumpierres@outlook.com>
2023-08-01 16:18:53 -0300

    adicionando importações

--- src/usecases/index.ts
+++ src/usecases/index.ts
@@ -1,3 +1,6 @@
 export * from './Users/createUser.usecase';
 export * from './Users/loginUser.usecase';
+export * from './Notes/deleteNote.usecase'
+export * from './Notes/listNote.usecase'
+export * from './Notes/updateNote.usecase'
 export * from './Notes/createNote.usecase'
