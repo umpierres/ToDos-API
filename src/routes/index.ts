40c86dc7d33f661d8081db67285102442f40244b
umpierres <alexandraumpierres@outlook.com>
2023-08-10 15:59:02 -0300

    trocando rotas para um arquivo paralelo

---
+++ src/routes/index.ts
@@ -0,0 +1,23 @@
+import express from "express";
+import { NoteController, UserController } from "../controllers";
+import { validateDataUser, validateNote, validateUserLogin } from "../middlewares";
+
+const app = express.Router();
+
+app.get('/', (req, res) => res.status(200).json({ message: 'OK' }));
+
+// user
+const userController = new UserController();
+
+app.post("/users/signup", validateDataUser, userController.signup)
+app.post("/users/signin", validateUserLogin, userController.signin)
+
+//notes
+const noteController = new NoteController();
+
+app.post("/notes/:ownerID", validateNote, noteController.create)
+app.get("/notes/:ownerID",) //listar
+app.put("/notes/:ownerID/:noteID", validateNote, noteController.update)
+app.delete("/notes/:ownerID/:noteID", noteController.delete)
+
+export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,33 +1,16 @@
 import cors from 'cors';
 import 'dotenv/config';
 import express from 'express';
-import { NoteController, UserController } from './controllers';
-import { validateDataUser } from './middlewares/';
-import { validateUserLogin } from './middlewares/';
-import {  validateNote } from './middlewares';
+import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({ extended:false }));
+app.use(routes);
 
 app.listen(process.env.PORT, () => {
     console.log(`Servidor rodando na porta: ${process.env.PORT}`)
 })
 
-app.get('/', (req, res) => res.status(200).json({ message: 'OK' }));
-
-// user
-const userController = new UserController();
-
-app.post("/users/signup", validateDataUser, userController.signup)
-app.post("/users/signin", validateUserLogin, userController.signin)
-
-//notes
-const noteController = new NoteController();
-
-app.post("/notes/:ownerID", validateNote, noteController.create)
-app.get("/notes/:ownerID",) //listar
-app.put("/notes/:ownerID/:noteID", validateNote, noteController.update)
-app.delete("/notes/:ownerID/:noteID", noteController.delete)
