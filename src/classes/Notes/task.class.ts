b509f61da97259aefdbd359b9feac632b8edfc93
umpierres <alexandraumpierres@outlook.com>
2023-08-10 15:35:39 -0300

    criando tipos e removendo setters

--- src/classes/Notes/task.class.ts
+++ src/classes/Notes/task.class.ts
@@ -1,75 +1,83 @@
 import { BaseClass } from "../BaseClass/baseClass.class";
-import { User } from "../User/user.class";
+import { User, UserJSON } from "../User/user.class";
+import { Response } from "express";
+
+type updateNoteDTO = {
+	title?: string,
+	description?: string,
+    favorite?: boolean, 
+	archived?: boolean, 
+    date: Date, 
+}
+
+export type NoteJSON = {
+    id: string
+    title:string,
+    description: string,
+    favorite: boolean,
+    archived: boolean,
+    date: Date, 
+    owner: Omit<UserJSON, 'password'>,
+}
 
 export class Note extends BaseClass {
-    private date: Date = new Date()
+    
+    private _date: Date
 	constructor(
-		private title: string,
-		private description: string,
-		private favorite: boolean = false,
-		private archived: boolean = false,
-		private owner: Omit<User, 'senha'>,
-  
-	) {
+        private _title: string,
+		private _description: string,
+		private _favorite: boolean = false,
+		private _archived: boolean = false,
+		private _owner: Omit<User, 'password'>,
+        ) {
 		super();
+        this._date = new Date()
 	}
 
-	toJSON() {
+
+	public toJSON() {
 		return {
-			id: this.getId(),
-			title: this.getTitle(),
-			description: this.getDescription(),
-			archived: this.getArchived(), 
-            owner: this.getOwner(),
-            favorite: this.getFavorite(), 
-            date: this.getDate(), 
+			id: this._id,
+			title: this._title,
+			description: this._description,
+            favorite: this._favorite, 
+			archived: this._archived, 
+            date: this._date, 
+            owner:{
+				id: this._owner.toJSON().id,
+				email: this._owner.toJSON().email,
+			},
 		};
 	}
 
-	getTitle(): string {
-        return this.title;
-    }
-
-    getDescription(): string {
-        return this.description;
-    }
-
-    getOwner(): Omit<User, 'senha'> {
-        return this.owner;
-    }
-
-    getArchived(): boolean {
-        return this.archived;
-    }
-
-    getFavorite(): boolean {
-        return this.favorite;
-    }
-
-    getDate(): Date {
-        return this.date;
-    }
-    setTitle(title: string): void {
-        this.title = title;
-    }
-
-    setDescription(description: string): void {
-        this.description = description;
-    }
-
-    setOwner(owner: Omit<User, 'senha'>): void  {
-        this.owner = owner;
-    }
-
-    setArchived(): void {
-        this.archived = !this.archived;
-    }
-
-    setFavorite(): void {
-        this.favorite = !this.favorite;
-    }
-
-    setDate(date: Date): void {
-        this.date = date;
+    public updateNote(newInfo: updateNoteDTO, res: Response) {
+        if(newInfo.title) {
+            if(newInfo.title.length < 3) {
+                return res
+                .status(400)
+                .json({ success: false, message: 'O titulo é muito curto.' });
+            }
+            this._title = newInfo.title
+        }
+        if(newInfo.description) {
+            if(newInfo.description.length < 3 || newInfo.description.length > 100) {
+                return res
+                .status(400)
+                .json({success: false, message: 'A descrição é muito curta ou muito longa.' });
+            }
+            this._description = newInfo.description
+        }
+        if(newInfo.archived) {
+            this._archived = newInfo.archived
+        }
+        if(newInfo.favorite) {
+            this._favorite = newInfo.favorite
+        }
+        if(newInfo.date) {
+            this._date = newInfo.date
+        }
+        return res
+        .status(200)
+        .json({success: true, message: 'Nota atualizada com sucesso!' });
     }
 }
